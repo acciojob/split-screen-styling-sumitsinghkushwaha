073954a29476759049b22e70fc556223849c0105
Sumit Singh Kushwaha <68112081+sumitsinghkushwaha@users.noreply.github.com>
2023-10-09 19:28:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-//your JS code here. If required.
 
-state: "failed",
-body: () => {
-  cy.get("#article").should("have.css", "flex", "3 1 0%");
-  cy.get("#aside").should("have.css", "flex", "1 1 0%");
-}
 
